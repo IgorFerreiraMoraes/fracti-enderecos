e8a7815674b985ea3e3cfaf9cfffb8d066f4e4aa
Wandering Raven <igorfm13524@gmail.com>
2024-02-11 11:59:48 -0300

    :recycle: refactor: Itera Formatos Com Loop #15
    Referente à issue #15
    
    Remove código redundante utilizado um laço de repetição para cehcar os padrões

--- fracti.py
+++ fracti.py
@@ -1,37 +1,34 @@
 import re
 
 
+# Expressões regulares para identificar diferentes formatos de endereço
+patterns = {
+    "indicator_street": r"^(.*?)(?=,?\s(?:No|n°|number)\s*\d+$)",
+    "indicator_number": r"(?:No|n°|number)\s*\d+$",
+    "national_street": r"^(.*?)(?=,?\s\d+\s*[a-zA-Z]?\b$)",
+    "national_number": r"(\d+\s*[a-zA-Z]?\b)$",
+    "inverted_street": r"^\d+\s*[a-zA-Z]?\b[,]?\s*",
+    "inverted_number": r"^(\d+(\s*[a-zA-Z]\b)?)(?=,?\s*\w)",
+}
+
+
 def split_address(input_address):
-    # Expressões regulares para identificar diferentes formatos de endereço
-    patterns = {
-        "indicator_street": r"^(.*?)(?=,?\s(?:No|n°|number)\s*\d+$)",
-        "indicator_number": r"(?:No|n°|number)\s*\d+$",
-        "national_street": r"^(.*?)(?=,?\s\d+\s*[a-zA-Z]?\b$)",
-        "national_number": r"(\d+\s*[a-zA-Z]?\b)$",
-        "inverted_street": r"^\d+\s*[a-zA-Z]?\b[,]?\s*",
-        "inverted_number": r"^(\d+(\s*[a-zA-Z]\b)?)(?=,?\s*\w)",
-    }
+    address_parts = set()
 
     # Verifica o formato do endereço e extrai a rua e o número correspondentes
-    if re.search(patterns["indicator_street"], input_address) and re.search(
-        patterns["indicator_number"], input_address
-    ):
-        street = re.search(patterns["indicator_street"], input_address).group()
-        number = re.search(patterns["indicator_number"], input_address).group()
-    elif re.search(patterns["national_street"], input_address) and re.search(
-        patterns["national_number"], input_address
-    ):
-        street = re.search(patterns["national_street"], input_address).group()
-        number = re.search(patterns["national_number"], input_address).group()
-    elif re.search(patterns["inverted_street"], input_address) and re.search(
-        patterns["inverted_number"], input_address
-    ):
-        street = re.split(patterns["inverted_street"], input_address)[1]
-        number = re.search(patterns["inverted_number"], input_address).group()
-    else:
-        return "Não foi possível processar o endereço"
+    for pattern_name, pattern_expression in patterns.items():
+        match = re.search(pattern_expression, input_address)
+        if pattern_name == "inverted_street":  # Diferente com número no começo
+            match = re.search(
+                re.split(pattern_expression, input_address)[1], input_address
+            )
+        if match:
+            address_parts.add(match.group())
+            if (
+                len(address_parts) == 2
+            ):  # Dois itens, rua e número, não é necessário continuar o loop
+                break
 
-    address_parts = [street, number]
     return address_parts
 
 
